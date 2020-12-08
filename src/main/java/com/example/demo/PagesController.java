872b941f7dd248325680f79c6a082df6064c5ebf
willjantscher <william.jantscher@gmail.com>
2020-12-08 12:09:26 -0700

    starting math in spring

--- src/main/java/com/example/demo/PagesController.java
+++ src/main/java/com/example/demo/PagesController.java
@@ -1,14 +1,34 @@
 package com.example.demo;
+//to run the server ./mvnw spring-boot:run
+
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //
 public class PagesController {
 
-        @GetMapping("/hello")       //path that will access the below method
-        public String helloWorld() {
-            return "Hello World";
-        }
-
+    @GetMapping("/hello")       //path that will access the below method
+    public String helloWorld() {
+        return "Hello World";
+    }
+    //GET /tasks HTTP/1.1
+    //Host: example.com
+    @GetMapping("/tasks")
+    public String getTasks() {
+        return "These are tasks";
+    }
+        //test method with curl -X GET http://localhost:8080/tasks
+    //POST /tasks HTTP/1.1
+    //Host: example.com
+    @PostMapping("/tasks")
+    public String createTask() {
+        return "You just POSTed to /tasks";
+    }
+        //curl -X POST http://localhost:8080/tasks
+    @GetMapping("/math/pi")
+    public float returnPi() {
+        return  3.141592653589793f;
+    }
 }
