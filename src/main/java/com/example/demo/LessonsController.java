11065b43f819344c59da06f8a66ff6cbab98c502
willjantscher <william.jantscher@gmail.com>
2020-12-10 12:34:04 -0700

    added a database to Spring

---
+++ src/main/java/com/example/demo/LessonsController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/lessons")
+public class LessonsController {
+
+    private final LessonRepository repository;
+
+    public LessonsController(LessonRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public Iterable<Lesson> all() {
+        return this.repository.findAll();
+    }
+
+    @PostMapping("")
+    public Lesson create(@RequestBody Lesson lesson) {
+        return this.repository.save(lesson);
+    }
+
+}
