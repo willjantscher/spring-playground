7ae04a4f1f2f8e0279328d5f24d524f6468be000
willjantscher <william.jantscher@gmail.com>
2020-12-10 13:54:29 -0700

    crud endpoints added

--- src/main/java/com/example/demo/LessonsController.java
+++ src/main/java/com/example/demo/LessonsController.java
@@ -2,11 +2,24 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/lessons")
 public class LessonsController {
 
     private final LessonRepository repository;
+
+    @GetMapping("/{id}")
+    public Optional<Lesson> show(@PathVariable int id) {
+        Long newId = new Long(id);
+        return this.repository.findById(newId);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        this.repository.deleteById(id);
+    }
+
 
     public LessonsController(LessonRepository repository) {
         this.repository = repository;
@@ -17,9 +30,13 @@ public class LessonsController {
         return this.repository.findAll();
     }
 
+
+
     @PostMapping("")
     public Lesson create(@RequestBody Lesson lesson) {
         return this.repository.save(lesson);
     }
+
+
 
 }
